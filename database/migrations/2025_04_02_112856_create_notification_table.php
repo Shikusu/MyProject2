18ee79ffe99d76020df97eaddef9fc836374a687
Shikusu <108171013+Shikusu@users.noreply.github.com>
2025-04-03 13:40:57 +0300

    how 'bout now

--- database/migrations/2025_04_02_112856_create_notification_table.php
+++ database/migrations/2025_04_02_112856_create_notification_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->text('message');
             $table->timestamps();
 
-            $table->foreign('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
         });
     }
 
